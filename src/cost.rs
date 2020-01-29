bd8590d4339e3706585074199462fbf228b106c0
ritchie46 <ritchie46@gmail.com>
2020-01-29 16:20:16 +0100

    working

--- src/cost.rs
+++ src/cost.rs
@@ -5,6 +5,49 @@ pub fn log_pdf(x: f64, mean: f64, std_dev: f64) -> f64 {
     (-0.5 * d * d) - consts::LN_SQRT_2PI - std_dev.ln()
 }
 
+pub fn mean(x: &[f64]) -> f64 {
+    x.iter().sum::<f64>() / x.len() as f64
+}
+
+pub fn var(x: &[f64]) -> f64 {
+    let mu = mean(x);
+    let mut sum_diff = 0.;
+
+    for v in x {
+        sum_diff += (v - mu).powf(2.)
+    }
+    sum_diff
+}
+
+fn median(numbers: &Vec<f64>) -> f64 {
+    let mut numbers: Vec<f64>= numbers.clone();
+    numbers.sort_by(|a, b| a.partial_cmp(b).unwrap());
+
+    let mid = numbers.len() / 2;
+    if numbers.len() % 2 == 0 {
+        mean(&vec![numbers[mid - 1], numbers[mid]]) as f64
+    } else {
+        numbers[mid]
+    }
+
+}
+
+pub fn l2(signal: &Vec<f64>, start: usize, end: usize) -> f64 {
+    var(&signal[start..end]) * (end - start) as f64
+}
+
+pub fn l1(signal: &Vec<f64>, start: usize, end: usize) -> f64 {
+    let sub: Vec<f64> = signal[start..end].to_vec();
+    let med = median(&sub);
+
+    let mut loss = vec!();
+    for s in sub.iter() {
+        loss.push((s - med).abs())
+    }
+    loss.iter().sum()
+}
+
+
 #[cfg(test)]
 mod tests {
     use super::*;
--- src/pelt.rs
+++ src/pelt.rs
@@ -1,4 +1,16 @@
 use crate::estimator::MutEstimator;
+use std::collections::HashMap;
+use crate::cost;
+
+macro_rules! dict(
+{ $($key:expr => $value:expr),+} => {{
+    let mut m = HashMap::new();
+    $(
+       m.insert($key, $value);
+    )+
+    m
+}}
+);
 
 #[derive(Debug)]
 struct Pelt {
@@ -27,6 +39,73 @@ impl Pelt {
             n_samples: 0,
             best_partition: None,
         }
+    }
+
+    fn segmentation(&self, lambda: f64, signal: &Vec<f64>) -> Vec<usize> {
+        let idx = proposed_idx(self.n_samples, self.jump, self.min_size);
+
+        // Maps (t, breakpoint) to loss + Lambda
+        let first_part = dict!((0, 0) => 0.);
+        // partitions[t] contains the optimal partition of signal[0:t]
+        let mut partitions_map = dict!(0 => first_part);
+
+        let min_size = self.min_size as f64;
+        let jump = self.jump as f64;
+        let mut admissible: Vec<usize> = vec![];
+
+        // bp: breakpoint
+        for bp in idx {
+            // Add points from 0 to current breakpoint as admissible
+            // For every slice t:breakpoint we will compute the loss
+            // and store it in a hashmap in partitions_map
+            let new_adm_pt = (((bp as f64 - min_size) / jump).floor() * jump) as usize;
+            admissible.push(new_adm_pt);
+
+            let mut subproblems = vec!();
+            for t in &admissible {
+
+                let tmp_part = partitions_map.get(t);
+                let mut tmp_part = match tmp_part {
+                    // First partition of 0:t doesn't yet exist
+                    None => {
+                        continue;
+                    }
+                    Some(v) => v.clone(),
+                };
+
+                let loss = cost::l1(signal, *t, bp as usize);
+                tmp_part.insert((*t, bp), loss + lambda);
+
+                subproblems.push(tmp_part);
+            }
+
+            // Find optimal partition and assign it to partitions_map
+            let mut min_part = &subproblems[0];
+            let mut min_val = 1e99;
+            for (i, d) in subproblems.iter().enumerate() {
+                let c = d.values().sum::<f64>();
+                if  c < min_val {
+                    min_val = c;
+                    min_part = &subproblems[i]
+                }
+            }
+            partitions_map.insert(bp, min_part.clone());
+
+            let mut temp_admissible = vec!();
+            let sum_part_bp: f64 = partitions_map.get(&bp).unwrap().values().sum();
+            for (t, partition) in admissible.iter().zip(subproblems) {
+                let c = partition.values().sum::<f64>();
+                if c < (sum_part_bp + lambda) {
+                    temp_admissible.push(*t);
+                }
+            }
+            admissible.clear();
+            admissible.append(&mut temp_admissible);
+        }
+        let best_part = &partitions_map[&self.n_samples];
+        let mut cp: Vec<usize> = best_part.keys().map(|(start, end)| *end).collect();
+        cp.sort();
+        cp[1..].to_vec()
     }
 }
 
@@ -80,5 +159,17 @@ mod test {
     fn test_proposed_idx() {
         println!("{:?}", proposed_idx(20, 5, 2));
         assert_eq!(proposed_idx(20, 5, 2), vec!(5, 10, 15, 20))
+    }
+
+    #[test]
+    fn test_segmentation() {
+        let mut p = Pelt::new(Some(5), Some(2));
+
+        let s = std::fs::read_to_string("signal.txt").unwrap();
+        let signal :Vec<f64> = s.split("\n").map(|x|x.parse().unwrap()).collect();
+        p.fit(&signal);
+        let cp = p.segmentation(10., &signal);
+        println!("{:?}", cp);
+        assert!(false)
     }
 }
